bffc63227bd983e0d1e2cbc411a04b93a4501ef5
Waihon Yew <yewwaihon@gmail.com>
2020-10-03 20:15:22 +0800

    Initial version

---
+++ crowdfund.rb
@@ -0,0 +1,8 @@
+project1 = "ABC"
+project2 = "LMN"
+project3 = "XYZ"
+
+fund1 = 1000
+
+puts "Project #{project1} has $#{fund1} in funding."
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
