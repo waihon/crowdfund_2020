053a0f2c320945c0dfe98aca41d1d039f4813ade
Waihon Yew <yewwaihon@gmail.com>
2020-10-04 00:08:23 +0800

    Variables and objects

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,8 +1,14 @@
 project1 = "ABC"
-project2 = "LMN"
-project3 = "XYZ"
-
 fund1 = 1000
+project2 = "LMN"
+fund2 = 3000
+project3 = "XYZ"
+fund3 = 2150
+project4 = "PQR"
+fund4 = 4800
 
-puts "Project #{project1} has $#{fund1} in funding."
-puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}\n\t#{project4}"
+puts "#{project1.upcase} has a fund of #{fund1}."
+puts "#{project2.upcase} has a health of #{fund2}."
+puts "#{project3.upcase} has a fund of #{fund3}.".center(50, "*")
+puts "#{project4.upcase.ljust(30, '.')} #{fund4} fund"
