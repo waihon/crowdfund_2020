0f0f2105cdbd3d8cd1a9cd47c0dfb4a42d546c9e
Waihon Yew <yewwaihon@gmail.com>
2020-10-17 22:41:51 +0800

    Code example: A project has an initial target funding amount

--- project_spec.rb
+++ project_spec.rb
@@ -2,12 +2,14 @@ require_relative 'project'
 
 describe Project do
 
-  it "has an initial target funding amount"
   before do
     @initial_fund = 100
     @project = Project.new("ABC", @initial_fund, @initial_fund * 50)
   end
 
+  it "has an initial target funding amount" do
+    expect(@project.current_fund).to eq(@initial_fund)
+  end
 
   it "computes the total funding outstanding as the target funding amount minus the funding amount"
 
