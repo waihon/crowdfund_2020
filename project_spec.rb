4e3070626747decf713483096d412a8e99f21e5e
Waihon Yew <yewwaihon@gmail.com>
2020-10-17 22:33:38 +0800

    Express the behaviors of a Project as a list of pending examples

---
+++ project_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'project'
+
+describe Project do
+
+  it "has an initial target funding amount"
+
+  it "computes the total funding outstanding as the target funding amount minus the funding amount"
+
+  it "increases funds by 25 when funds are added"
+
+  it "decreases funds by 15 when funds are removed"
+
+  it "has a default value of 0 for funding amount"
+
+end
